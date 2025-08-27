e9593228fa5f2904b96ba89e7239e0d76115f343
Sunny BIARD <sunny.biard@gmail.com>
2025-08-27 18:01:05 +0200

    Implementation du dashboard

---
+++ src/app/core/models/FormattedData.ts
@@ -0,0 +1,5 @@
+export interface FormattedData {
+    
+    name: string;
+    value: number;
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import { FormattedData } from 'src/app/core/models/FormattedData';
+import { Olympic } from 'src/app/core/models/Olympic';
 
 @Component({
   selector: 'app-home',
@@ -10,9 +12,57 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 export class HomeComponent implements OnInit {
   public olympics$: Observable<any> = of(null);
 
+  view: [number, number] = [700, 400];
+  results: FormattedData[] = [];
+  gradient: boolean = false;
+  showLegend: boolean = false;
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
+  trimLabels: boolean = false;
+
+  numberOfCountries!: number;
+  numberOfJOs!: number;
+
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
+    
     this.olympics$ = this.olympicService.getOlympics();
+
+    this.olympics$.subscribe((data) => {
+      
+      if (data) {
+        
+        this.results = this.formatData(data);
+        this.numberOfCountries = data.length;
+        this.numberOfJOs = data[0].participations.length;
+      }
+    });
+  }
+
+  formatData(data: Olympic[]) {
+    
+    let formattedData: FormattedData[] = [];
+
+    data.forEach((country) => {
+
+      let medals = 0;
+
+      if (country.participations) {
+
+        medals = country.participations.reduce((sum, participation) => sum + participation.medalsCount, 0);
+      }
+
+      formattedData.push({
+        name: country.country,
+        value: medals
+      })
+    })
+
+    return formattedData;
+  }
+
+  onSelect(name: string): void {
+
   }
 }
